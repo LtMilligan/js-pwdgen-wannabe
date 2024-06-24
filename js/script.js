5c8dd4b4d3c5584614a5bbfc65a5e39a04003e92
LtMilligan <gioele.miscia@gmail.com>
2024-06-24 15:31:33 +0200

    [UPD] Exsercise completed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt('Inserisci il tuo nome');
+let lastName = prompt('Inserisci il tuo cognome');
+let prefColor = prompt('Inserisci il tuo colore preferito');
+let firstNumb = prompt('Inserisci un numero ad una cifra');
+let secondNumb = prompt('Inserisci un altro numero ad una cifra');
+let multiNumb = firstNumb * secondNumb;
+let password = `${firstName}${lastName}${prefColor}${multiNumb}`;
+document.getElementById('pass-gen').innerHTML = 'La tua password generata è: ' + password;
