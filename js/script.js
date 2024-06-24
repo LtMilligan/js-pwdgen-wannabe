4d037f5f99e337c0113694673a4ac0220683ee37
LtMilligan <gioele.miscia@gmail.com>
2024-06-24 15:44:24 +0200

    [UPD] Colored innerHTML

--- js/script.js
+++ js/script.js
@@ -5,4 +5,4 @@ let firstNumb = prompt('Inserisci un numero ad una cifra');
 let secondNumb = prompt('Inserisci un altro numero ad una cifra');
 let multiNumb = firstNumb * secondNumb;
 let password = `${firstName}${lastName}${prefColor}${multiNumb}`;
-document.getElementById('pass-gen').innerHTML = 'La tua password generata è: ' + password;
+document.getElementById('pass-gen').innerHTML = `La tua password generata è: <span class="text-primary">${password}</span>`;
